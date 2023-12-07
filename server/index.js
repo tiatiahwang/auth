eb0a466ed16256471bd5b043c208a4372b141005
tiatiahwang <tiahwang44@gmail.com>
2023-12-06 16:59:17 -0800

    add: db connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log('connected to mongoDB'))
+  .catch((e) => console.log('DB connection error: ', e));
 
 const app = express();
 
-app.listen(3000, () => console.log('Server 3000'));
+app.listen(3000, () =>
+  console.log('Server running on 3000'),
+);
