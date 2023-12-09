a95cb20616e7f3c8558098ffa138d50838c194c8
tiatiahwang <tiahwang44@gmail.com>
2023-12-08 23:06:52 -0800

    add: redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App.jsx';
 import './index.css';
-import { store } from './redux/store.js';
+import { store, persistor } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
